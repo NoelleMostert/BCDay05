7ea1531ee5123dd868259afb0fe7dbb7719e9b30
Noelle Mostert <noellemostert@gmail.com>
2018-02-20 01:29:33 +0200

    fixed ex07

--- ex07.c
+++ ex07.c
@@ -7,42 +7,29 @@ char *ft_strcapitalize(char *str)
     int upper;
     
     i = 0;
-    upper = 1;
+    upper = 0;
     while(str[i] != '\0')
     {        
-        //if(i == 0)
-        //    str[i] = str[i] - 32;
-        if(str[i] >= 'a' && str[i] <= 'z' && upper == 1)
+        if(str[i] >= '0' && str[i] <= '9')
+            upper = 0;
+        else if((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
             {
-                str[i] = str[i] - 32;
-                upper = 0;
-            }
-        else if(str[i] >= 'A' && str[i] <= 'Z' && upper == 0)
-            {
-                str[i] = str[i] + 32;
-                upper = 0;
-            }
-        else if (str[i] >= '0' && str[i] <= '9' && upper == 0)
-            {
+                if (i == 0 || upper == 1)
+                    str[i] = str[i] - 32;
                 upper = 0;
             }
         else
             {
                 upper = 1;
             }
-            
-        //if(str[i] == 32)
-          //  upper = 1;
-        //else
-          //  upper = 0;
-            i++;
+        i++;
     }
     return(str);
 }
 
 int main()
 {
-   char haystack[100] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+   char haystack[100] = " salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
    char *ret;
 
    ret = ft_strcapitalize(haystack);
